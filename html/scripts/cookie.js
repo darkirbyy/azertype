8de1a858b0d7e7e8dfbdef94b42794a58b65a7e0
darkirby <darkirby.tuta.com>
2025-12-04 20:18:13 +0100

    Use localStorage instead of cookie

--- html/scripts/cookie.js
+++ html/scripts/cookie.js
@@ -1,21 +1,3 @@
-
-function GetCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-function SetCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; SameSite=Strict; path=" + API_COOKIE_PATH;
-}
-
 const Cookie = {
     game_id: 0,
     played: false,
@@ -25,15 +7,15 @@ const Cookie = {
 
     reinit: function () {
         this.game_id = 0,
-            this.played = false,
-            this.liste_mot = [],
-            this.seconds_total = undefined,
-            this.seconds_par_mot = []
+        this.played = false,
+        this.liste_mot = [],
+        this.seconds_total = undefined,
+        this.seconds_par_mot = []
     },
 
     read: function () {
-        const cookieValue = GetCookie('azertype');
-        const myArray = cookieValue != undefined ? JSON.parse(cookieValue) : [0, false, [], undefined, []];
+        const storageValue = localStorage.getItem('azertype/last_game');
+        const myArray = storageValue != undefined ? JSON.parse(storageValue) : [0, false, [], undefined, []];
         this.game_id = myArray[0];
         this.played = myArray[1];
         this.liste_mot = myArray[2];
@@ -43,7 +25,7 @@ const Cookie = {
 
     write: function () {
         const myArray = [this.game_id, this.played, this.liste_mot, this.seconds_total, this.seconds_par_mot];
-        SetCookie('azertype', JSON.stringify(myArray), 1);
+        localStorage.setItem('azertype/last_game', JSON.stringify(myArray));
     }
 }
 
